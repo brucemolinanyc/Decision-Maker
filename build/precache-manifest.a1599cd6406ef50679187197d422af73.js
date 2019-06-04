9c395f71747b0581699f4894d5925b0cfe65925e
brucemolinanyc <brucemolinanyc@gmail.com>
2019-06-04 18:00:26 -0400

    move project to new heroku and git repo

---
+++ build/precache-manifest.a1599cd6406ef50679187197d422af73.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5308c51f91c55774e01774581f2cd75",
+    "url": "/index.html"
+  },
+  {
+    "revision": "304cb278190559a7d438",
+    "url": "/static/css/2.e95dff61.chunk.css"
+  },
+  {
+    "revision": "51f06a061e5ebd7cc7c4",
+    "url": "/static/css/main.4f611a62.chunk.css"
+  },
+  {
+    "revision": "304cb278190559a7d438",
+    "url": "/static/js/2.b1e541f1.chunk.js"
+  },
+  {
+    "revision": "51f06a061e5ebd7cc7c4",
+    "url": "/static/js/main.bbdbb3c1.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
